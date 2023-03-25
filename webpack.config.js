9dcff6a8aa0efa80ed3bf1220f5448408212be67
makafreitasdev <matheus_maka@hotmail.com>
2023-03-25 11:38:48 -0300

    criando estutura do projeto e configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const modoDev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: modoDev ? 'development' : 'production',
+
+  entry: {
+    main: [
+      './src/main.js'
+    ]
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  devtool: 'eval-source-map',
+
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }, {
+      test: /\.(png|svg|jpg|gif)$/i,
+      use: ['file-loader']
+    }, {
+      test: /\.html$/i,
+      type: "assets/resource"
+    }
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "assets/css/style.css"
+    }),
+
+    new CopyPlugin({
+      patterns: [{
+        context: path.resolve(__dirname),
+        from: "./src",
+        to: path.resolve(__dirname, 'public')
+      }, {
+        from: "./src/assets/js",
+        to: path.resolve(__dirname, 'public/assets/js')
+      }]
+    }),
+    //new HtmlWebpackPlugin()
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        }
+      }),
+      new HtmlMinimizerPlugin()
+    ]
+  },
+
+  devServer: {
+    static: {
+      directory: ("./public")
+    },
+    compress: true,
+    port: 9000
+  }
+};
